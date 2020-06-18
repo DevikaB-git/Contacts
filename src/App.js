6544e447bcccbd8bf79461cec411e941e4d6b15e
DevikaB-git <devikaznair111@gmail.com>
2020-06-18 21:28:54 +0530

    commit all

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Dropdown from './Component/Dropdown';
+
 class App extends Component {
   render() {
     return (
+
       <div className="container" >
         <Dropdown />
       </div>
